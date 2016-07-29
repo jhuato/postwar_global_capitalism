8574a03459182d59b91c2f9f560e53d32ed4b714
jhuato <jhuato.sfc@gmail.com>
2016-07-29 08:31:09 -0400

    Delete basic_matrix_ops.R

--- basic_matrix_ops.R
+++
@@ -1,8 +0,0 @@
-# Experimenting with R
-# Julio Huato
-A <- matrix(c(1,2,3,0),nrow=2,ncol=2) # filled by column
-B <- matrix(c(0,3,2,1),nrow=2,ncol=2,byrow=T) # filled by row
-C<-A%*%B # matrix multiplication
-x=c(5,2)
-t(C) # transpose of C
-10*A # Hadamard product
